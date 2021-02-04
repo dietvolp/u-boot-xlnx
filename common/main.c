903937a15bc3104f4c4081ada238d3820caba41b
Dieter Volpert <dieter.volpert@yahoo.com>
2021-02-04 03:46:32 +0100

    main fix

--- common/main.c
+++ common/main.c
@@ -60,7 +60,7 @@ void main_loop(void)
 		cli_secure_boot_cmd(s);
 
 
-	setenv("sdboot", "echo Running script from SD... && mmcinfo && fatload mmc 0 0x2000000 u-boot.scr && source 0x2000000");
+	env_set("sdboot", "echo Importing environment from SD... && mmcinfo && load mmc 0 0x2000000 uEnv.txt && env import -t 0x2000000 ${filesize} && boot");
 
 	autoboot_command(s);
 
